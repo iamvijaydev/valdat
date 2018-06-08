c846898a221f64f287c6e27d81adb0a8e5aca8c1
Vijay Dev <vijaydev.in@gmail.com>
2018-06-08 13:40:42 +0530

    added docs for Validate.ts

--- lib/Validate.ts
+++ lib/Validate.ts
@@ -1,29 +1,73 @@
+/**
+ * Interface for generic object with string key an any value.
+ */
 export interface IData {
     [propName: string]: any
 }
 
+/**
+ * Interface for error object returned by a validator function.
+ */
 export interface IError {
+    /**
+     * Indicating if error exists.
+     */
     error: boolean;
+
+    /**
+     * Indicating the error message.
+     * It should be empty when error is false
+     */
     message: string;
 }
 
+/**
+ * Interface for the validator function
+ */
 export interface IValidator {
+    /**
+     * @param data The whole data object passed along with schema
+     * @param key Key in the data object to checked
+     * @returns The error object IError
+     */
     (data: IData, key: string): IError;
 }
 
+/**
+ * Interface for Validate class
+ */
 export interface IValidate {
+    /**
+     * Set "required" to true, indicating that the value to be validated should exist.
+     * @returns "this" for currying.
+     */
     isRequired(): IValidate;
 }
 
+/**
+ * Base class for validation
+ */
 export default class Validate implements IValidate {
-    protected required: boolean;
+
+    /**
+     * Array of validators functions pushed to test the value.
+     */
     protected stack: Function[];
+    
+    /**
+     * The flag which indicate if the value to be validated should exist.
+     * If set true then the value cannot be undefined.
+     */
+    protected required: boolean;
 
     constructor() {
         this.required = false;
         this.stack = [];
     }
 
+    /**
+     * @inheritdoc
+     */
     isRequired(): IValidate {
         this.required = true;
         return this;
